d8657fd87fab3e9b65fb5467fc40ee782623e6a4
shourovahmd <moh.abuhanifa@gmail.com>
2022-10-08 17:23:14 +0600

    added

---
+++ index.js
@@ -0,0 +1,34 @@
+let createId = () => {
+  let s4 = () => {
+    return Math.floor((1 + Math.random()) * 0x10000)
+      .toString(16)
+      .substring(1);
+  };
+  //return id of format 'aaaaaaaa'-'aaaa'-'aaaa'-'aaaa'-'aaaaaaaaaaaa'
+  return `${
+    s4() + s4() + "-" + s4() + "-" + s4() + "-" + s4() + "-" + s4() + s4()
+  }`;
+};
+
+console.log(createId());
+
+//*   for react  *//
+
+// import { useMemo } from "react";
+
+// let createId = () => {
+//   let s4 = () => {
+//     return Math.floor((1 + Math.random()) * 0x10000)
+//       .toString(16)
+//       .substring(1);
+//   };
+//   //return id of format 'aaaaaaaa'-'aaaa'-'aaaa'-'aaaa'-'aaaaaaaaaaaa'
+//   return `${s4() + "-" + s4() + "-" + s4() + "-" + s4()}`;
+// };
+
+// const useGenerateId = () => {
+//   const id = useMemo(() => createId(), []);
+//   return `${id}`;
+// };
+
+// export default useGenerateId;
